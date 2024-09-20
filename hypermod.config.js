704a20ce4fd5167fa3e7982755e5dfd53ce73c7e
Daniel Del Core <ddelcore@atlassian.com>
2024-09-20 12:33:24 +1000

    map options as per traditional jscodeshift API

--- hypermod.config.js
+++ hypermod.config.js
@@ -1,7 +1,13 @@
 import { codemods } from "module-replacements-codemods";
 
 const config = {
-  presets: codemods,
+  presets: Object.keys(codemods).reduce((acc, key) => {
+    acc[key] = (file, { jscodeshift: j }, options) => {
+      const transform = codemods[key](options);
+      return transform(file, { j });
+    }
+    return acc;
+  });
 };
 
 export default config;
