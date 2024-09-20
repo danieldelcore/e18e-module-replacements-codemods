8af5dcc90e7cc8eb583c8bc336a311c767836feb
Daniel Del Core <ddelcore@atlassian.com>
2024-09-20 12:43:33 +1000

    fix syntax err

--- hypermod.config.js
+++ hypermod.config.js
@@ -3,7 +3,7 @@ import { codemods } from "module-replacements-codemods";
 const config = {
   presets: Object.keys(codemods).reduce((acc, key) => {
     acc[key] = (file, { jscodeshift: j }, options) => {
-      const transform = codemods[key](options);
+      const { transform } = codemods[key](options);
       return transform(file, { j });
     };
     return acc;
